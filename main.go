47c2ed9e4e31bfefaad32fdc7801a80d962c7870
Sahil <sahildeo616@gmail.com>
2026-06-15 23:31:35 +0530

    ADDED: SQLITE DB SUPPORT WITH INSERT AND SELECT

--- main.go
+++ main.go
@@ -2,53 +2,130 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Music struct {
+	ID         int `gorm:"primarykey"`
+	Name       string
+	Link       string
+	IsPlaylist bool
+}
 
-func main(){
-	
+func main() {
+
+	db, err := gorm.Open(sqlite.Open("db/data.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = db.AutoMigrate(&Music{})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	for {
 
 		fmt.Println("\nWelcome mpv control\n")
-		
-		
+
 		fmt.Println("Options:")
-	
+
 		fmt.Println("1. Play music.")
 		fmt.Println("2. Add music.")
 		fmt.Println("3. Remove music.")
 		fmt.Println("4. Exit music")
-		
+
 		var entry int
 		fmt.Scan(&entry)
-	
+
 		switch entry {
-		
-			case 1:
-				fmt.Println("Play music")
-				cmd := exec.Command("mpv", "--no-video", "--loop-playlist", "https://music.youtube.com/playlist?list=PLoiMA0z1W8dmbPKuu4dErfS4hOFi97M1L")
-				cmd.Stdin = os.Stdin
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				
-				err := cmd.Run(); 
-				if err != nil {
-					fmt.Println("mpv exited with error:", err) 
-				}
-				
-			case 2:
-				fmt.Println("Add music")
-				break
-			case 3:
-				fmt.Println("Remove music")
-				break
-			case 4:
-				fmt.Println("Exiting...")
-				return 
-			default:
-				fmt.Println("Invalid Option")	
+
+		case 1:
+			fmt.Println("Play music")
+
+			var music []Music
+			result := db.Find(&music)
+
+			if result.Error != nil {
+				log.Fatal(result.Error)
+			}
+
+			log.Println(result.RowsAffected)
+
+			var id int
+			fmt.Println("Enter ID to play: ")
+			fmt.Scan(&id)
+
+			var musicRow Music
+			db.First(&music, "id=?", id)
+
+			var loop string
+			if musicRow.IsPlaylist {
+				loop = "--loop-playlist"
+			} else {
+				loop = "--loop"
+			}
+
+			cmd := exec.Command("mpv", "--no-video", loop, musicRow.Link)
+
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println("mpv exited with error:", err)
+			}
+
+		case 2:
+			fmt.Println("Add music")
+			var name, link, isPlaylist string
+			var music Music
+			fmt.Println("Name: ")
+			fmt.Scan(&name)
+			fmt.Println("Link: ")
+			fmt.Scan(&link)
+			fmt.Println("Playlist(P)/Single(S)[Default=P]: ")
+			fmt.Scan(&isPlaylist)
+
+			if isPlaylist == "S" || isPlaylist == "s" {
+				fmt.Println("Single")
+
+				music.Name = name
+				music.Link = link
+				music.IsPlaylist = false
+			} else {
+				fmt.Println("Playlist")
+
+				music.Name = name
+				music.Link = link
+				music.IsPlaylist = true
+			}
+
+			result := db.Create(&music)
+			if result.Error != nil {
+				log.Fatal(result.Error)
+				return
+			}
+
+			log.Println("Inserted id: ", music.ID)
+
+		case 3:
+			fmt.Println("Remove music")
+		case 4:
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Invalid Option")
 		}
-	}	
+	}
 }
